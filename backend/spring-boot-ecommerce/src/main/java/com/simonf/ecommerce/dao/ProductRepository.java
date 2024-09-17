81831ab0fd314c6cf726f407dbc1b472c4905aeb
Simon Flohr <sflohr2@wgu.edu>
2024-09-16 20:21:54 -0500

    Add CORS support to the Spring Boot project
    - Add the `@CrossOrigin()` annotation to both `ProductRepository` and `ProductCategoryRepository` in order to enable CORS support.

--- backend/spring-boot-ecommerce/src/main/java/com/simonf/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/simonf/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.simonf.ecommerce.dao;
 
 import com.simonf.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // Endpoint for product repository is automatically configured by Spring Data REST
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
