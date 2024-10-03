f48f1cbb494d5adc49bf60f1959875b43576fbed
Simon Flohr <sflohr2@wgu.edu>
2024-10-03 17:44:01 -0500

    Define our Angular routes and assign them to the RouterModule

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -6,6 +6,16 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import { Routes, RouterModule } from '@angular/router';
+
+// Define our routes in a const variable
+const routes:Routes = [
+  {path:'category/:id', component: ProductListComponent},
+  {path:'category', component: ProductListComponent},
+  {path:'products', component: ProductListComponent},
+  {path:'', redirectTo:'/products', pathMatch:'full'},
+  {path:'**', redirectTo:'/products', pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
@@ -13,6 +23,7 @@ import { ProductService } from './services/product.service';
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes), // Assign the routes from const variable 'routes' to our imported RouterModule
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
