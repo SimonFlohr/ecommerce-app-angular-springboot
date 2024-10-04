2231bee47a9a7b31e9c0d7f0c073c3380989a60c
Simon Flohr <sflohr2@wgu.edu>
2024-10-03 20:47:02 -0500

    Implement `id` parameter functionality in Spring Boot's `ProductRepository`

--- backend/spring-boot-ecommerce/src/main/java/com/simonf/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/simonf/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,14 @@
 package com.simonf.ecommerce.dao;
 
 import com.simonf.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 // Endpoint for product repository is automatically configured by Spring Data REST
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
