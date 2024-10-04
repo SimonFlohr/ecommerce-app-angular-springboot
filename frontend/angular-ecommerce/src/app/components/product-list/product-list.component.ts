932b763743420732757368bdd1590dae4b54f194
Simon Flohr <sflohr2@wgu.edu>
2024-10-03 20:27:45 -0500

    Incorporate `category id` functionality
    In `ProductListComponent`:
    - Create a new `currentCategoryId` variable.
    - Initialize the current active route that loaded the view component.
    - Subscribe the activated route the the `listProducts()` function.
    - Enhance `listProducts()` with internal id handling.
    
    In `ProductService`:
    - Create a `theCategoryId` parameter.

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -9,15 +10,33 @@ import { Product } from '../../common/product';
 })
 export class ProductListComponent {
   products:Product[] = [];
+  currentCategoryId:number = 1; // keep track of current category number
+  
+  constructor(private productService:ProductService,
+              private route:ActivatedRoute) {} // initialize the current active route that loaded the component
 
-  constructor(private productService:ProductService) {}
-
+  // Subscribe the actived route to the `listProducts()` function
   ngOnInit():void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList()
+
+    // Check if `id` parameter is available
+    const hasCategoryId:boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // Get the `id` parameter string. Convert string to a number using the `+` symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!; // in this context, `!` is the non-null assertion operator
+    } else {
+      // No category id available, default to category 1
+      this.currentCategoryId = 1;
+    }
+    
+
+    this.productService.getProductList(this.currentCategoryId)
       .subscribe( data => { this.products = data; } );
   }
 }
